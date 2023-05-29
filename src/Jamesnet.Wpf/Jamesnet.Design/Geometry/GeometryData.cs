195dfbdb840ad669c7cce80017b09f8e0e87337c
JAEWUNG LEE <james@jamesnet.dev>
2023-05-29 22:58:04 +0900

    Updates

---
+++ src/Jamesnet.Wpf/Jamesnet.Design/Geometry/GeometryData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jamesnet.Design.Geometry
+{
+    public class GeometryData
+    {
+        public static string CheckDecagram => GeometryConverter.GetData();
+
+        public static string Email => GeometryConverter.GetData();
+        public static string EmailOutLine => GeometryConverter.GetData();
+
+        public static string Twitter => GeometryConverter.GetData();
+    }
+}
--- src/Jamesnet.Wpf/Jamesnet.Wpf/Global/Wpf/JamesIcon.cs
+++ src/Jamesnet.Wpf/Jamesnet.Wpf/Global/Wpf/JamesIcon.cs
@@ -1,14 +1,66 @@
 ﻿using Jamesnet.Design.Geometry;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Jamesnet.Wpf.Controls
 {
-    public partial class JamesIcon
+    public enum IconType
+    { 
+        None,
+
+        CheckDecagram,
+
+        Email,
+        EmailOutLine,
+
+        Twitter,
+    }
+
+    public class JamesIcon : Label
     {
-        public static string CheckDecagram => GeometryConverter.GetData();
+        public static DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(IconType), typeof(JamesIcon), new PropertyMetadata(IconType.None, IconPropertyChanged));
+        public static DependencyProperty FillProperty = DependencyProperty.Register("Fill", typeof(SolidColorBrush), typeof(JamesIcon), new PropertyMetadata(Brushes.Silver));
+        public static DependencyProperty DataProperty = DependencyProperty.Register("Data", typeof(Geometry), typeof(JamesIcon), new PropertyMetadata(null));
 
-        public static string Email => GeometryConverter.GetData();
-        public static string EmailOutLine => GeometryConverter.GetData();
+        public IconType Icon
+        {
+            get => (IconType)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
 
-        public static string Twitter => GeometryConverter.GetData();
+        public SolidColorBrush Fill
+        {
+            get => (SolidColorBrush)GetValue(FillProperty);
+            set => SetValue(FillProperty, value);
+        }
+
+        public Geometry Data
+        {
+            get => (Geometry)GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
+        }
+
+        private static void IconPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            JamesIcon jamesIcon = (JamesIcon)d;
+            string geometryData = "";
+
+            switch (jamesIcon.Icon)
+            {
+                case IconType.Twitter: geometryData = GeometryData.Twitter; break;
+                case IconType.CheckDecagram: geometryData = GeometryData.CheckDecagram; break;
+                case IconType.Email: geometryData = GeometryData.Email; break;
+                case IconType.EmailOutLine: geometryData = GeometryData.EmailOutLine; break;
+            }
+
+            jamesIcon.Data = Geometry.Parse(geometryData);
+        }
+
+        static JamesIcon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(JamesIcon), new FrameworkPropertyMetadata(typeof(JamesIcon)));
+        }
     }
 }
