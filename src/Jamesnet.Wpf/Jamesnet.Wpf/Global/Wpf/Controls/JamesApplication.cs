0571b4fca9e53cf758870a7e118d9d1e29634449
lukewire <lukewire129@gmail.com>
2023-10-17 17:48:58 +0900

    ㅡ

--- src/Jamesnet.Wpf/Jamesnet.Wpf/Global/Wpf/Controls/JamesApplication.cs
+++ src/Jamesnet.Wpf/Jamesnet.Wpf/Global/Wpf/Controls/JamesApplication.cs
@@ -9,86 +9,85 @@ using System.Collections.Generic;
 using System.Windows;
 using Unity;
 
-namespace Jamesnet.Wpf.Controls
+namespace Jamesnet.Wpf.Controls;
+
+public abstract class JamesApplication : PrismApplication
 {
-    public abstract class JamesApplication : PrismApplication
+    private List<IModule> _modules = new();
+    private object theme;
+
+    public JamesApplication()
     {
-        private List<IModule> _modules = new();
-        private object theme;
 
-        public JamesApplication()
-        {
-
-        }
-
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            ViewModelLocatorCollection items = new();
-            RegisterWireDataContexts(items);
-
-            base.OnStartup(e);
-        }
-
-        public JamesApplication AddInversionModule<T>() where T : IModule, new()
-        {
-            IModule module = new T();
-            _modules.Add(module);
-
-            return this;
-        }
-
-        public JamesApplication AddWireDataContext<T>() where T : ViewModelLocationScenario, new()
-        {
-            ViewModelLocationScenario scenario = new T();
-            _ = scenario.Publish();
-
-            return this;
-        }
-
-        protected virtual void RegisterWireDataContexts(ViewModelLocatorCollection items)
-        {
-        }
-
-        protected override void RegisterTypes(IContainerRegistry containerRegistry)
-        {
-            containerRegistry.RegisterInstance(this);
-            containerRegistry.RegisterSingleton<ContentManager>();
-            containerRegistry.RegisterSingleton<IEventHub, EventAggregatorHub>();
-
-            if (theme != null)
-            {
-                containerRegistry.RegisterInstance(theme as ResourceInitializer);
-                containerRegistry.RegisterSingleton<ResourceManager>();
-                ResourceManager themeManager = GetService<ResourceManager>();
-            }
-
-            foreach (IModule module in _modules)
-            {
-                module.RegisterTypes(containerRegistry);
-            }
-        }
-
-        public JamesApplication InitializeTheme<T>() where T : ResourceInitializer, new()
-        {
-            theme = new T();
-            return this;
-        }
-
-    public static T GetService<T>()
-    {
-        if (JamesApplication.Current is JamesApplication app)
-        {
-            return app.Container.Resolve<T>();
-        }
-        return default;
     }
 
-    public static ResourceDictionary Resource()
+    protected override void OnStartup(StartupEventArgs e)
     {
-        if (JamesApplication.Current is JamesApplication app)
-        {
-            return app.Resources;
-        }
-        return default;
+        ViewModelLocatorCollection items = new();
+        RegisterWireDataContexts(items);
+
+        base.OnStartup(e);
     }
+
+    public JamesApplication AddInversionModule<T>() where T : IModule, new()
+    {
+        IModule module = new T();
+        _modules.Add(module);
+
+        return this;
+    }
+
+    public JamesApplication AddWireDataContext<T>() where T : ViewModelLocationScenario, new()
+    {
+        ViewModelLocationScenario scenario = new T();
+        _ = scenario.Publish();
+
+        return this;
+    }
+
+    protected virtual void RegisterWireDataContexts(ViewModelLocatorCollection items)
+    {
+    }
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        containerRegistry.RegisterInstance(this);
+        containerRegistry.RegisterSingleton<ContentManager>();
+        containerRegistry.RegisterSingleton<IEventHub, EventAggregatorHub>();
+
+        if (theme != null)
+        {
+            containerRegistry.RegisterInstance(theme as ResourceInitializer);
+            containerRegistry.RegisterSingleton<ResourceManager>();
+            ResourceManager themeManager = GetService<ResourceManager>();
+        }
+
+        foreach (IModule module in _modules)
+        {
+            module.RegisterTypes(containerRegistry);
+        }
+    }
+
+    public JamesApplication InitializeTheme<T>() where T : ResourceInitializer, new()
+    {
+        theme = new T();
+        return this;
+    }
+
+public static T GetService<T>()
+{
+    if (JamesApplication.Current is JamesApplication app)
+    {
+        return app.Container.Resolve<T>();
+    }
+    return default;
+}
+
+public static ResourceDictionary Resource()
+{
+    if (JamesApplication.Current is JamesApplication app)
+    {
+        return app.Resources;
+    }
+    return default;
 }
