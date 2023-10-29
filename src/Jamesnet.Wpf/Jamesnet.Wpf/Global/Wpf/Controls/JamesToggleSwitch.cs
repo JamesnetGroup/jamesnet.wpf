dc1b38d2541ef9f6d60f6fefabb6debd73096c6c
JAEWUNG LEE <james@jamesnet.dev>
2023-10-29 12:08:19 +0900

    Updates

--- src/Jamesnet.Wpf/Jamesnet.Wpf/Global/Wpf/Controls/JamesToggleSwitch.cs
+++ src/Jamesnet.Wpf/Jamesnet.Wpf/Global/Wpf/Controls/JamesToggleSwitch.cs
@@ -1,9 +1,24 @@
-﻿using System.Windows.Controls.Primitives;
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
 
 namespace Jamesnet.Wpf.Controls
 {
     public class JamesToggleSwitch : ToggleButton
     {
+        #region CornerRadius
 
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register(
+                "CornerRadius",
+                typeof(CornerRadius),
+                typeof(JamesToggleSwitch),
+                new FrameworkPropertyMetadata());
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+        #endregion
     }
 }
